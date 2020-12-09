edded2412d47484b6901f263fe8522eb21793fc2
squidinkettle <jchabol309@hotmail.com>
2020-12-09 15:59:59 +0100

    Added Waves of Enemies and Cutscene

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] GameObject[] cameras;
+    float timeStart = 25.45f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameras[1].SetActive(true);
+        cameras[0].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeStart)
+        {
+            cameras[0].SetActive(true);
+            cameras[1].SetActive(false);
+
+
+        }
+    }
+}
