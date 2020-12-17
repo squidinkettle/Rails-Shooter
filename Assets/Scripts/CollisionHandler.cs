20fdddc12c00659b782c64359f674d7d3609c99e
squidinkettle <jchabol309@hotmail.com>
2020-12-17 17:14:45 +0100

    Added Bloom Effect

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CollisionHandler : MonoBehaviour
 {
     [Header("General")]
     [SerializeField] int maxPlayerHealth = 100;
     [SerializeField]int playerHealth;
+    [SerializeField] Text playerHealthText;
 
     [Header("Particle Control")]
     [Tooltip("Particle FX prefab on player")][SerializeField] GameObject deathFX;
@@ -16,13 +18,15 @@ public class CollisionHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerHealthText.text = "Health: " + maxPlayerHealth.ToString();
         playerHealth = maxPlayerHealth;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         playerHealth -= 20;
-        if(playerHealth<=0)
+        playerHealthText.text = "Health: " + playerHealth.ToString();
+        if (playerHealth<=0)
             StartDeathSequence();
     }
 
